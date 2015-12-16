2ce47ee78f7a23194a0a28fcffb4ef0483d91f27
Axel Voitier <axel.voitier@gmail.com>
2015-12-16 02:33:03 +0100

    Finally compiling\!...

---
+++ python-openzwave:master/docker_install.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -e
+
+buildDeps='
+g++
+gcc
+libudev-dev
+make
+unzip
+wget
+'
+ 
+apt-get update
+apt-get install -y --no-install-recommends $buildDeps
+
+pip install --no-cache-dir louie cython==0.21.1
+
+cd /tmp
+wget https://github.com/OpenZWave/python-openzwave/archive/master.zip
+unzip master.zip
+cd /tmp/python-openzwave-master
+mkdir .git # Necessary if you don't want to see a weird error about .pyx files when doing make install...
+
+wget https://github.com/OpenZWave/open-zwave/archive/master.zip
+unzip master.zip
+mv open-zwave-master openzwave
+
+make build
+make install
+
+find /usr/local \( -type d -a -name test -o -name tests \) -o \( -type f -a -name '*.pyc' -o -name '*.pyo' \) -exec rm -rf '{}' +
+apt-get purge -y --auto-remove $buildDeps
+rm -rf /var/lib/apt/lists/*
+rm -rf /root/.cache/pip
+
+cd /tmp
+rm master.zip
+rm -R python-openzwave-master
